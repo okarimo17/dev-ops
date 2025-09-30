3f81f8dbb8613644f3f61caf6417e53e1ea361c2
okarimo17 <oucifkarim01@gmail.com>
2025-09-30 12:30:52 +0100

    testing stack swarm with secrets

--- shared/server/index.js
+++ shared/server/index.js
@@ -21,6 +21,20 @@ server.get("/test", (req, res) => {
     container: os.hostname()
   });
 });
+const fs = require('fs');
+
+server.get("/sec", (req, res) => {
+  const SECRET_PATH = '/run/secrets/db_password';
+  let dbPassword = null;
+  try {
+    // Read the content of the file synchronously (safe to do during app startup)
+    dbPassword = fs.readFileSync(SECRET_PATH, 'utf8').trim();
+    return res.json({pass: dbPassword})
+    // process.env.DB_PASSWORD = dbPassword;    
+  } catch (error) {
+    return res.status(404).json({error: `Error reading secret at ${SECRET_PATH}:  ${error.message}`})
+  }
+});
 
 server.listen(SERVER_PORT, ()=>{
     console.log(`Server is running on : http://localhost:${SERVER_PORT}`)
